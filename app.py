cfead55f4adfd957aee15c45f137bb39164eba77
Aditya Prasad <109896394+Oneaboveall18@users.noreply.github.com>
2023-03-10 12:19:08 +0000

    updated the layout

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ anime_episodes = anime['airing_episodes']
 anime_season = anime['season']
 anime_score = anime['average_score']
 anime_genres = anime['genres']
-anime_description = anime['desc']
 anime_next_ep = anime['next_airing_ep']
-
-anime_info = f"English Name: {anime_en_name}\n\nJapanese Name: {anime_romaji_name}\n\nRunning Status: {anime_runtime_start} - {anime_runtime_end}\n\nFormat: {anime_format}\n\nStatus: {anime_status}\n\nEpisodes: {anime_episodes}\n\nSeason: {anime_season}\n\nScore: {anime_score}\n\nGenres: {anime_genres}\n\nSynopsis: \n{anime_description}"
-
+anime_description = anime['desc']
+anime_description = anime_description.replace("<br>", "")
+anime_description = anime_description.replace("<i>", "")
+anime_description = anime_description.replace("</i>", "")
